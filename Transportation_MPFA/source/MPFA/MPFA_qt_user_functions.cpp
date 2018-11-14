019bfd140768288c3073b6b246cc0e7bab664ce7
lukey <lukey11@unm.edu>
2018-11-13 17:20:51 -0700

    Updates for doing experiments

--- Transportation_MPFA/source/MPFA/MPFA_qt_user_functions.cpp
+++ Transportation_MPFA/source/MPFA/MPFA_qt_user_functions.cpp
@@ -80,7 +80,7 @@ for (size_t i=0; i< loopFunctions.Nests.size(); i++){
     	// Draw the nest on the arena
 
 	    //DrawCircle(nest_3d, CQuaternion(), loopFunctions.NestRadius, CColor::GREEN);
-     DrawCylinder(nest_3d, CQuaternion(), loopFunctions.NestRadius, 0.05, CColor::GREEN);
+     DrawCylinder(nest_3d, CQuaternion(), loopFunctions.NestRadius, 0.015, CColor::GREEN);
     }
 }
 
