df87d04a30f5f845276b047a9d76141e14a7ba40
lukey15 <lqcobra@hotmail.com>
2018-11-22 21:26:54 -0700

    Add the MPFA transportation project. 2. Updated the lib in dynamic MPFA

---
+++ Transportation_MPFA/build/source/Base/CMakeFiles/Nest_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BaseController_automoc.cpp"
+  "Pheromone_automoc.cpp"
+  "Nest_automoc.cpp"
+  "CMakeFiles/Nest_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Nest_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Transportation_MPFA/build/source/Base/CMakeFiles/Pheromone_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BaseController_automoc.cpp"
+  "Pheromone_automoc.cpp"
+  "Nest_automoc.cpp"
+  "CMakeFiles/Pheromone_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Pheromone_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
