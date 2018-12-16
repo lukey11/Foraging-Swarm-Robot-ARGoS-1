e8e96d76baab72467306d580fd7ea7b8c29cfe14
lukey11 <lukey@BCLABs-iMac.local>
2018-12-16 16:43:08 -0700

    Update experiment files

--- Static_MPFA/experiments.py
+++ Static_MPFA/experiments.py
@@ -12,10 +12,10 @@ class Random_Argos:
 if __name__ == "__main__":
     #system = 'linux' if platform.system() == 'Linux' else 'mac'
     #files =['Random_MPFA_r1192_d256_tag16384_80by80.xml']
-    #files = ['Random_MPFA_r1192_d256_tag16384_80by80.xml', 'Random_MPFA_r296_d64_tag4096_40by40.xml', 'Random_MPFA_r2_d1_tag64_5by5.xml', 'Random_MPFA_r16_d4_tag256_10by10.xml', 'Random_MPFA_r72_d16_tag1024_20by20.xml']
+    #files = ['Random_MPFA_r1192_d257_tag16384_80by80.xml', 'Random_MPFA_r296_d65_tag4096_40by40.xml', 'Random_MPFA_r16_d5_tag256_10by10.xml', 'Random_MPFA_r72_d17_tag1024_20by20.xml']
     #files = ['Random_MPFA_r192_d65_tag64_40by40.xml', 'Random_MPFA_r192_d65_tag256_40by40.xml', 'Random_MPFA_r192_d65_tag1024_40by40.xml', 'Random_MPFA_r384_d65_tag16384_40by40.xml']
-    files = ['Random_MPFA_r384_d65_tag16384_40by40.xml']
-    run_count = 5
+    files = ['Random_MPFA_r296_d65_tag4096_40by40.xml']
+    run_count = 2
     for file in files:
         print file
         this_run = Random_Argos("./experiments/"+file)
