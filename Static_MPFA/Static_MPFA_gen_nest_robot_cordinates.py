38a289648e527ecfc4b1fd1cd0b4dd24549231fb
lukey15 <lqcobra@hotmail.com>
2019-02-01 22:13:19 -0700

    add xml files

--- Static_MPFA/Static_MPFA_gen_nest_robot_cordinates.py
+++ Static_MPFA/Static_MPFA_gen_nest_robot_cordinates.py
@@ -17,7 +17,7 @@ def sub_gen_coord(max_x,min_x, k):
     #print coordinates
     return coordinates
 
-arena_width = 64
+arena_width = 128
 
 max_x, max_y = arena_width/2.0,  arena_width/2.0;
 min_x, min_y = -max_x, -max_y;
@@ -45,32 +45,39 @@ coord_info_text.close()
 count =0;
 
 total_robot=0
+speed = 0.16
+num_Drobot = 1
+forageRate = 110/1800.0
+print "forageRate=", forageRate
+#unit = np.sqrt(2*((gaps[-1]/2.0)**2))
+#unit = np.sqrt(2*((1/2.0)**2))
 
-num_Drobot = 4
-unit = np.sqrt(2*((gaps[-1]/2.0)**2))
-for coords in results[:-1]:
-    for xy in coords:
-        distance = np.sqrt(xy[0]**2 + xy[1]**2)
-        print "distance=", distance
-        print "unit=", unit
-        print round(distance/unit)
-        quantity = num_Drobot*round(distance/unit);
-        print "quantity", quantity
-        total_robot += quantity
+
+#for coords in results[:-1]:
+#    for xy in coords:
+#        distance = np.sqrt(xy[0]**2 + xy[1]**2)
+#        print "distance=", distance
+#        #print "unit=", unit
+#        print round(2*distance/0.16)
+#        quantity = num_Drobot*round(distance/unit)
+#        print "quantity", quantity
+#        if quantity > 30:
+#            quantity = 30
+#        total_robot += quantity
 	    
-        coord_info.write("<distribute>\n")
-        coord_info.write("<position max=\"" + str(xy[0]+gaps[-1]/4.0)+ ", " + str(xy[1]+gaps[-1]/4.0) + ", 0.0\" method=\"uniform\" min=\"" + str(xy[0]-gaps[-1]/4.0)+ ", " + str(xy[1]-gaps[-1]/4.0) + ", 0.0\"/>\n")
-        coord_info.write("<orientation mean=\"0, 0, 0\" method=\"gaussian\" std_dev=\"360, 0, 0\"/>\n")
-        coord_info.write("<entity max_trials=\"100\" quantity=\""  + str(int(quantity))  +  "\">\n")
-        coord_info.write("<foot-bot id=\"D" + str(count)+ "-" + "\"><controller config=\"MPFA\"/></foot-bot>\n")
-        coord_info.write("</entity>\n")
-        coord_info.write("</distribute>\n\n")
-        count += 1  
+#        coord_info.write("<distribute>\n")
+#        coord_info.write("<position max=\"" + str(xy[0]+gaps[-1]/4.0)+ ", " + str(xy[1]+gaps[-1]/4.0) + ", 0.0\" method=\"uniform\" min=\"" + str(xy[0]-gaps[-1]/4.0)+ ", " + str(xy[1]-gaps[-1]/4.0) + ", 0.0\"/>\n")
+#        coord_info.write("<orientation mean=\"0, 0, 0\" method=\"gaussian\" std_dev=\"360, 0, 0\"/>\n")
+#        coord_info.write("<entity max_trials=\"100\" quantity=\""  + str(int(quantity))  +  "\">\n")
+#        coord_info.write("<foot-bot id=\"D" + str(count)+ "-" + "\"><controller config=\"MPFA\"/></foot-bot>\n")
+#        coord_info.write("</entity>\n")
+#        coord_info.write("</distribute>\n\n")
+#        count += 1  
         
 # there are foraging and delivering robots in each region
 for xy in results[-1]:
     coord_info.write("<distribute>\n")
-    coord_info.write("<position max=\"" + str(xy[0])+ ", " + str(xy[1]) + ", 0.0\" method=\"uniform\" min=\"" + str(xy[0]-0.5)+ ", " + str(xy[1]-0.5) + ", 0.0\"/>\n")
+    coord_info.write("<position max=\"" + str(xy[0]+gaps[-1]/4.0)+ ", " + str(xy[1]+gaps[-1]/4.0) + ", 0.0\" method=\"uniform\" min=\"" + str(xy[0]-gaps[-1]/4.0)+ ", " + str(xy[1]-gaps[-1]/4.0) + ", 0.0\"/>\n")
     coord_info.write("<orientation mean=\"0, 0, 0\" method=\"gaussian\" std_dev=\"360, 0, 0\"/>\n")
     coord_info.write("<entity max_trials=\"100\" quantity=\"4\">\n")
     coord_info.write("<foot-bot id=\"F" + str(count)+ "-" + "\"><controller config=\"MPFA\"/></foot-bot>\n")
@@ -82,9 +89,12 @@ for xy in results[-1]:
     print "nest location =[",xy[0],", ",xy[1], "]"
     distance = np.sqrt(xy[0]**2 + xy[1]**2)
     print "distance=", distance
-    print "unit=", unit
-    print round(distance/unit)
-    quantity = num_Drobot*round(distance/unit);
+    #print "unit=", unit
+    print (forageRate*2*distance)/speed
+    quantity = math.ceil((forageRate*2*distance)/speed)
+    
+    if quantity > 30:
+        quantity = 30
     print "quantity", quantity
     total_robot += quantity
     
