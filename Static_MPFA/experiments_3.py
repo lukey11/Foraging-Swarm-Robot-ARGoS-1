066ec42ca41b0b03bef97da51fba488357361e10
lukey15 <lqcobra@hotmail.com>
2019-02-02 20:51:54 -0700

    update xml files

--- Static_MPFA/experiments_3.py
+++ Static_MPFA/experiments_3.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     #files = ['Random_MPFA_r1192_d256_tag16384_80by80.xml', 'Random_MPFA_r296_d64_tag4096_40by40.xml', 'Random_MPFA_r2_d1_tag64_5by5.xml', 'Random_MPFA_r16_d4_tag256_10by10.xml', 'Random_MPFA_r72_d16_tag1024_20by20.xml']
     #files = ['Random_MPFA_r548_d50_tag6147_49by49.xml']
     #files = ['1_8_19_constant_speed/Random_MPFA_r16_d5_tag20_4by4.xml', '1_8_19_constant_speed/Random_MPFA_rXXX_d17_tag328_16by16.xml']
-    files = ['1_19_31_vary_speed/Random_MPFA_64by64_mapTo_rXXX_d257_tag16384_128by128.xml', '1_19_31_vary_speed/Random_MPFA_r20_d5_tag4_2by2.xml', '1_19_31_vary_speed/Random_MPFA_rXXX_d17_tag64_8by8.xml', '1_19_31_vary_speed/Random_MPFA_rXXX_d65_tag1024_32by32.xml']
+    #files = ['Widths_32_48_64_96/Test_Random_MPFA_r24_d5_tag82_8by8.xml', 'Widths_32_48_64_96/Test_Random_MPFA_r16_d5_tag328_16by16.xml', 'Widths_32_48_64_96/Test_Random_MPFA_r16_d5_tag737_24by24.xml']
+    #files = ['1_8_19_constant_speed/Test_no_deliver_Random_MPFA_r16_d5_tag82_8by8.xml', '1_8_19_constant_speed/Test_no_deliver_Random_MPFA_r16_d5_tag328_16by16.xml', '1_8_19_constant_speed/Test_no_deliver_Random_MPFA_r16_d5_tag415_18by18.xml']
+    files = ['1_31_19_vary_speed/no_delivery_Random_MPFA_16by16_mapTo_rXXX_d65_tag1024_32by32.xml', '1_31_19_vary_speed/no_delivery_Random_MPFA_64by64_mapTo_rXXX_d257_tag16384_128by128.xml', '1_31_19_vary_speed/no_delivery_Random_MPFA_r16_d5_tag4_2by2.xml', '1_31_19_vary_speed/no_delivery_Random_MPFA_r64_d17_tag64_8by8.xml', '1_31_19_vary_speed/Random_MPFA_16by16_mapTo_rXXX_d65_tag1024_32by32.xml', '1_31_19_vary_speed/Random_MPFA_64by64_mapTo_rXXX_d257_tag16384_128by128.xml', '1_31_19_vary_speed/Random_MPFA_r20_d5_tag4_2by2.xml', '1_31_19_vary_speed/Random_MPFA_rXXX_d17_tag64_8by8.xml']
     run_count = 5
     for file in files:
         print file
