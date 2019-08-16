c30ede6326045144156de16790a793296951b3e2
lukey11 <lukey@bclabs-imac.unm.edu>
2019-08-15 20:49:26 -0600

    additional checking on the collision of MPFA without collision

--- Static_MPFA/source/MPFA/MPFA_qt_user_functions.cpp
+++ Static_MPFA/source/MPFA/MPFA_qt_user_functions.cpp
@@ -40,8 +40,8 @@ void MPFA_qt_user_functions::DrawOnRobot(CFootBotEntity& entity) {
 		//GetOpenGLWidget().renderText(0.0, 0.0, 0.5,             // position
 		//			     entity.GetId().c_str()); // text
 		
-			//DrawText(CVector3(0.0, 0.0, 0.3),   // position
-            //entity.GetId().c_str()); // text draw robot ID
+        DrawText(CVector3(0.0, 0.0, 0.3),   // position
+        entity.GetId().c_str()); // text draw robot ID
 		/* Restore face culling */
 		glEnable(GL_CULL_FACE);
 		/* Restore lighting */
