01f639b8c259530525d6806257ad8f3ed060fcc5
lukey15 <lqcobra@hotmail.com>
2021-02-17 16:16:13 -0700

    remove the export. No such a path

--- CPFA/build.sh
+++ CPFA/build.sh
@@ -6,7 +6,7 @@
 #fi
 
 cd build
-export PKG_CONFIG_PATH=/opt/local/argos3/2.8.12.2/gcc/5.4.0/lib/pkgconfig
+#export PKG_CONFIG_PATH=/opt/local/argos3/2.8.12.2/gcc/5.4.0/lib/pkgconfig
 echo "Configuring Makefiles with CMAKE..."
 cmake -DBUILD_EVOLVER=NO -DCMAKE_BUILD_TYPE=Release ..
 
