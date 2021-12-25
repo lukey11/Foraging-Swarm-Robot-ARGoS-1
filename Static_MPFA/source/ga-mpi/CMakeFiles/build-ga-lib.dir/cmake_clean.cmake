58d2479e83623140b02389026ad8493b3cf227d2
hui <hsun130187>
2021-12-25 12:58:01 -0700

    delete old files

--- Static_MPFA/source/ga-mpi/CMakeFiles/build-ga-lib.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/build-ga-lib"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/build-ga-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
